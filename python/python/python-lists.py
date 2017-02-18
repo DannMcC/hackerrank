84278b3ad7437ed0c390c3dec3d7bcb742d036d5
Dan McCune <megadannn@gmail.com>
2017-02-18 18:20:26 -0500

    ...

--- python/python/python-lists.py
+++ python/python/python-lists.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     N = int(input())
     list = []
-    for i in range (N):
+    for i in range(N):
         command = input().split(' ')
         if command[0] == 'insert':
             list.insert(int(command[1]), int(command[2]))
