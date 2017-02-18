3e1c126550a8e1f39abd75175c8eb49af781f7b0
Dan McCune <megadannn@gmail.com>
2017-02-18 18:19:47 -0500

    lists

---
+++ python/python/python-lists.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    N = int(input())
+    list = []
+    for i in range (N):
+        command = input().split(' ')
+        if command[0] == 'insert':
+            list.insert(int(command[1]), int(command[2]))
+        if command[0] == 'print':
+            print(list)
+        if command[0] == 'remove':
+            list.remove(int(command[1]))
+        if command[0] == 'append':
+            list.append(int(command[1]))
+        if command[0] == 'sort':
+            list.sort()
+        if command[0] == 'pop':
+            list.pop()
+        if command[0] == 'reverse':
+            list.reverse()
